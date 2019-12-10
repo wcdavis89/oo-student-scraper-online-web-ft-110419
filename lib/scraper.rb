e1867a5248bf2cf8f5e0d218d475f928bb76342d
Whittney Davis <wcdavis89@yahoo.com>
2019-12-10 20:02:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,4 +22,26 @@ class Scraper
     end
     students
   end
+  
+  def self.scrape_profile_page(profile_url)
+
+    profile_html = open(profile_url)
+    profile_doc = Nokogiri::HTML(profile_html)
+    attributes = {}
+    profile_doc.css("div.social-icon-container a").each do |link_xml|
+      case link_xml.attribute("href").value
+      when /twitter/
+        attributes[:twitter] = link_xml.attribute("href").value
+      when /github/
+        attributes[:github] = link_xml.attribute("href").value
+      when /linkedin/
+        attributes[:linkedin] = link_xml.attribute("href").value
+      else
+          attributes[:blog] = link_xml.attribute("href").value
+      end
+    end
+    attributes[:profile_quote] = profile_doc.css("div.profile-quote").text
+    attributes[:bio] = profile_doc.css("div.bio-content div.description-holder").text.strip
+    attributes
+  end
 end
